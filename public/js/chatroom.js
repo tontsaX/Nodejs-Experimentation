cf7ef75f5ff2f91a247caae9b6570aee6b978cd2
Toni Kainulainen <tonikainulainen.ttk@hotmail.fi>
2021-03-01 17:30:52 +0200

    fixed addresss

--- public/js/chatroom.js
+++ public/js/chatroom.js
@@ -1,6 +1,6 @@
 (function connect(){
-    let socket = io.connect('http://localhost:3000');
-    
+    //let socket = io.connect('http://localhost:3000');
+    let socket = io.connect('/');
     let username = document.querySelector('#username');
     let usernameBtn = document.querySelector('#usernameBtn');
     let curUsername = document.querySelector('.card-header');
