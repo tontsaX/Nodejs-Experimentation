1905006e1399f3fd7c04a32369165189ad54b023
Toni Kainulainen <tonikainulainen.ttk@hotmail.fi>
2021-03-02 17:35:36 +0200

    Added authenticatation requirement

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const {ensureAuthenticated} = require("../config/auth.js");
+
 // login page
 /* when client calls address /welcome, the server renders
 welcome.ejs page */
@@ -13,7 +15,7 @@ router.get('/register', (req,res) => {
     res.render('register');
 });
 
-router.get('/dashboard', (req, res) => {
+router.get('/dashboard', ensureAuthenticated, (req, res) => {
     res.render('dashboard', {
         user: req.user
     });
