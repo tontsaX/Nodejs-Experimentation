1b11ce97dfd9f90c7dc345e14cfd49e9dbe796c1
Toni Kainulainen <tonikainulainen.ttk@hotmail.fi>
2021-03-02 14:35:22 +0200

    fixed paths again

--- routes/users.js
+++ routes/users.js
@@ -13,7 +13,7 @@ router.get('/register', (req, res) => {
 });
 
 // Register handle
-router.post('logintuto/users/register', (req, res) => {
+router.post('/register', (req, res) => {
     const {name, email, password, password2} = req.body;
     let errors = [];
     console.log(' Name ' + name + ' email: ' + email + ' pass: ' + password);
