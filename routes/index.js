b3e56148fd2650728a4a72efebcae85c427683cb
Toni Kainulainen <tonikainulainen.ttk@hotmail.fi>
2021-03-02 12:05:27 +0200

    index.js and users.js files added

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+// login page
+/* when client calls address /welcome, the server renders
+welcome.ejs page */
+router.get('/welcome', (req,res) => {
+    res.render('welcome');
+});
+
+// register page
+router.get('/register', (req,res) => {
+    res.render('register');
+});
+
+// Export the router instance so that it can be used in other files.
+module.exports = router;
