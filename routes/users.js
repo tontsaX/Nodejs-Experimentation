8c8fba5e531d544860a2f89121e70ed8a3cbaef9
Toni Kainulainen <tonikainulainen.ttk@hotmail.fi>
2021-03-02 14:10:18 +0200

    The first step of adding registration property to register function

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,5 @@
+// ladataan user model käyttöön
+const User = require('../models/user.js');
 const express = require('express');
 const router = express.Router();
 
@@ -12,6 +14,48 @@ router.get('/register', (req, res) => {
 
 // Register handle
 router.post('/register', (req, res) => {
+    const {name, email, password, password2} = req.body;
+    let errors = [];
+    console.log(' Name ' + name + ' email: ' + email + ' pass: ' + password);
+    
+    if(!name || !email || !password || !password2) {
+        errors.push({msg: "Please fill in all fields."});
+    }
+    
+    // check if match
+    if(password != password2) {
+        errors.push({msg: "passwords don't match"});
+    }
+    
+    // check if password is more than 6 characters
+    if(password.length < 6) {
+        errors.push({msg: "password atleast 6 characters"});
+    }
+    if(errors.length > 0) {
+        res.render('register', {
+            errors: errors,
+            name: name,
+            email: email,
+            password: password,
+            password2: password2
+        });
+    } else {
+        // validation passed
+        User.findOne({email: email}).exec((err, user) => {
+            console.log(user);
+            if(user) {
+                errors.push({msg: "email already registered"});
+                render(res,errors,name,email,password,password2);
+            } else {
+                const newUser = new User({
+                    name: name,
+                    email: email,
+                    password: password
+                });
+            }
+        })
+    }
+
 });
 
 router.post('/login', (req, res, next) => {
