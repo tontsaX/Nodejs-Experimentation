b353c5333cda62aaea23e4fa1d328cacdda4c0c7
Toni Kainulainen <tonikainulainen.ttk@hotmail.fi>
2021-04-17 19:08:00 +0300

    Prepare for logout tests.

--- routes/socketing.js
+++ routes/socketing.js
@@ -2,17 +2,12 @@ const express = require('express');
 const router = express.Router();
 const socketio = require('socket.io');
 const {ensureAuthenticated} = require("../config/auth");
-var chatroom = ''; /* tätä ei voi käyttää, koska se vaihtuu joka kerta
-                    * kun chat-osoitteeseen otetaan yhteyttä
-                    */
 
 router.get('/', (req, res) => {
 	
 });
 
-// http://localhost:3000/chathall/chatroom-
 router.get('/chatroom-:chatName', ensureAuthenticated, (req, res) => {
-	chatroom = req.params.chatName;
     res.render('chatroom', {
 		user: req.user,
 		chatroom: req.params.chatName
@@ -23,18 +18,17 @@ const serverExport = require('../config/server');
 const io = socketio(serverExport.server);
 
 io.on('connection', socket => {
-
-    //socket.join(chatroom);
+    var chatroom = '';
     
     socket.on('chatroom', data => {
         socket.join(data.chatroom);
         console.log("Chatroom from client: " + data.chatroom);
+        chatroom = data.chatroom;
     });
     
     console.log("New user connected.")
 	//console.log("Chatroom from client: " + chatroom);
-	console.dir(socket.rooms);
-	//console.log("Connect count: " + );
+	//console.log("Connect count: " + );kissakala
 	console.log("=============");
     
     // message received from the client
@@ -47,7 +41,7 @@ io.on('connection', socket => {
     socket.on('typing', data => {
         /*"When we use broadcast, every user except the one who is typing the message receives the typing event from the server."*/
         //socket.to(chatroom).emit('typing', {username: socket.username});
-        socket.broadcast.emit('typing', {username: socket.username});
+        socket.to(chatroom).broadcast.emit('typing', {username: socket.username}); // mikä on socket.username
     });
 })
 
