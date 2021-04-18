302b894175b21e51bda32b046b3c55202133ff88
Toni Kainulainen <tonikainulainen.ttk@hotmail.fi>
2021-04-18 17:59:39 +0300

    Made rejectUnauthorized true. Now the app shouldn't be in risk of MITM attack.

--- config/config.js
+++ config/config.js
@@ -25,7 +25,7 @@ module.exports = {
     "dialectOptions": {
         ssl: {
             require: true,
-            rejectUnauthorized: false
+            //rejectUnauthorized: false
         }
     }
   },
