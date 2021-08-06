d3bc16cdcead3a934c49a924518913412e94ee0e
Toni Kainulainen <tonikainulainen.ttk@hotmail.fi>
2021-08-06 14:00:51 +0300

    jj

--- routes/users.js
+++ routes/users.js
@@ -16,6 +16,12 @@ router.get('/register', (req, res) => {
     res.render('register');
 });
 
+// uses a regex to check if email is valid
+function isValidEmail(email) {
+    let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
 // Register handle
 router.post('/register', async function(req, res) {
     const {name, email, password, password2} = req.body;
